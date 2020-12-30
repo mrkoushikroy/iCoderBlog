b1a3374db3f737086210adc55d19169f9051c0a5
Koushik <koushikroy.buie@gmail.com>
2020-12-30 23:30:37 +0530

    commiting on 30.12.20

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,26 @@
+from django.core import paginator
 from home import models
 from django.shortcuts import redirect, render, HttpResponse
 from blog.models import Post, BlogComment
 from django.contrib import messages
 from blog.templatetags import extras
+from django.core.paginator import Paginator
 
 # Create your views here.
 def blogHome(request):
-    allPost = Post.objects.all()
-    context={'allPost': allPost}
-    return render(request, 'blog/blogHome.html', context)
+    allPost = Post.objects.all().order_by('sno')
+    paginator = Paginator(allPost, 2)
+    page_no = request.GET.get('page')
+    page_obj = paginator.get_page(page_no)
+    # pager = {'page_obj':page_obj}
+    # context={'allPost': allPost}
+    return render(request, 'blog/blogHome.html', {'page_obj':page_obj})
 
 def blogPost(request, slug):
-    allPost = Post.objects.all()
+    allPost = Post.objects.all().order_by('sno')
+    paginator = Paginator(allPost, 4)
+    page_no = request.GET.get('page')
+    page_obj = paginator.get_page(page_no)
     post = Post.objects.filter(slug=slug).first()
     post.views= post.views +1
     post.save()
@@ -23,7 +32,7 @@ def blogPost(request, slug):
             replyDict[reply.parent.sno] = [reply]
         else:
             replyDict[reply.parent.sno].append(reply)
-    context={'post': post, 'allPost': allPost, 'comments':comments, 'user':request.user, 'replyDict':replyDict}
+    context={'post': post, 'allPost': allPost, 'comments':comments, 'user':request.user, 'replyDict':replyDict, 'page_obj':page_obj}
     # return HttpResponse(f'This is blogPost: {slug}')
     return render(request, 'blog/blogPost.html', context)
 
